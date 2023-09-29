556bbaeaeb253e988b08a1c34363652ca8eec1fb
hrssilva <haroldo102011@hotmail.com>
2023-09-29 17:59:56 -0300

    ontop-rdf4j dependency fix

--- src/main/java/com/souslesens/Jowl/model/ontopSparqlToSql.java
+++ src/main/java/com/souslesens/Jowl/model/ontopSparqlToSql.java
@@ -1,5 +1,16 @@
 package com.souslesens.Jowl.model;
 
+import it.unibz.inf.ontop.injection.OntopSQLOWLAPIConfiguration;
+import it.unibz.inf.ontop.rdf4j.repository.OntopRepository;
+import it.unibz.inf.ontop.rdf4j.repository.OntopRepositoryConnection;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.GraphQueryResult;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+
 public class ontopSparqlToSql {
-    
+
 }
