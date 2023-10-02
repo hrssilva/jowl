9081a2b6d36e279de522938887057532a10b1388
hrssilva <haroldo102011@hotmail.com>
2023-10-02 16:27:30 -0300

    small refactor

--- src/main/java/com/souslesens/Jowl/Controller/OntopController.java
+++ src/main/java/com/souslesens/Jowl/Controller/OntopController.java
@@ -10,7 +10,7 @@ import com.souslesens.Jowl.model.ontopSparqlToSqlInput;
 @RestController
 @RequestMapping("/sparql2sql")
 public class OntopController {
-    //Check repos
+    //Check repos - ontopSparqlToSql.getRepos();
     //  create repo or  -   OntopService.ontopInitRepo(ontopSparqlToSqlInput input);
     //  create sql request  -   OntopService.ontopSPARQL2SQL(String reqEncoded64, OntopRepository repo);
 
