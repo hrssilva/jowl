c5d9fcfec4fd8fa6b8043789316ea88a52586e1b
gustavo lacroix <gustavolacroix@gmail.com>
2023-10-10 16:39:47 -0300

    OntopController structure

--- src/main/java/com/souslesens/Jowl/Controller/OntopController.java
+++ src/main/java/com/souslesens/Jowl/Controller/OntopController.java
@@ -10,8 +10,41 @@ import com.souslesens.Jowl.model.ontopSparqlToSqlInput;
 @RestController
 @RequestMapping("/sparql2sql")
 public class OntopController {
-    //Check repos - ontopSparqlToSql.getRepos();
-    //  create repo or  -   OntopService.ontopInitRepo(ontopSparqlToSqlInput input);
-    //  create sql request  -   OntopService.ontopSPARQL2SQL(String reqEncoded64, OntopRepository repo);
+    
+    @Autowired
+        OntopService ontopService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> sparql2sql(@RequestBody ontopSparqlToSqlInput request) {
+
+        String reqEncoded64 = request.getSparqlReqEncoded64();
+        String repoURI = request.getOntologyURIEncoded64();
+        OntopRepository repo = ontopSparqlToSql.getRepo(repoURI);
+
+        // Add parameter count check?
+
+        if (repo == null) {
+            //  create repo - OntopService.ontopInitRepo(ontopSparqlToSqlInput input);
+            repo = ontopService.ontopInitRepo(request);
+            // Add repo do hash
+            ontopSparqlToSql.addRepo(repoURI, repo);
+        } 
+
+        //  create sql request - OntopService.ontopSPARQL2SQL(String reqEncoded64, OntopRepository repo);
+        String response = ontopService.ontopSPARQL2SQL(reqEncoded64, repo);
+
+        return ResponseEntity.ok(response);
+
+    }
+
+    private int countParams(Object... parameters) {
+        int count = 0;
+        for (Object param : parameters) {
+            if (param != null && !param.toString().isEmpty()) {
+                count++;
+            }
+        }
+        return count;
+    }
 
 }
