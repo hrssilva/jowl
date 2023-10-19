3a5b00f135a468e4f11cb65b0753a5fccccbefd3
hrssilva <haroldo.rssilva@gmail.com>
2023-10-19 15:29:24 -0300

    added missing imports

--- src/main/java/com/souslesens/Jowl/Controller/OntopController.java
+++ src/main/java/com/souslesens/Jowl/Controller/OntopController.java
@@ -1,11 +1,26 @@
 package com.souslesens.Jowl.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 import com.souslesens.Jowl.services.OntopService;
 import com.souslesens.Jowl.model.ontopSparqlToSql;
 import com.souslesens.Jowl.model.ontopSparqlToSqlInput;
+
+import it.unibz.inf.ontop.rdf4j.repository.OntopRepository;
+
 
 @RestController
 @RequestMapping("/sparql2sql")
