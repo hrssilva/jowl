0e5fa50bb0425b35b1b3b96c0baeafa54d8f0b6f
Joao Pedro Jensen Ourique <joao.ourique@aprix.com.br>
2023-11-21 20:14:25 -0300

    healthcheck: turned request into a post

--- healthcheck/healthcheck.js
+++ healthcheck/healthcheck.js
@@ -1,20 +1,27 @@
-const axios = require('axios');
-const base64 = require('base-64');
+const axios = require("axios");
+const base64 = require("base-64");
 
 const HEALTCHECK_URL = "http://example.com/healthcheck";
 const PARAM_VALUE = "http://check.com/healthcheck";
 const encodedParam = base64.encode(PARAM_VALUE);
+const body = {
+  sparqlReqEncoded64: encodedParam,
+  ontologyURIEncoded64: encodedParam,
+  ontologyContentEncoded64: encodedParam,
+  mappingsEncoded64: encodedParam,
+};
 
-const url = `${HEALTCHECK_URL}?encoded_param=${encodedParam}`;
-
-axios.get(url)
-  .then(response => {
+axios
+  .post(HEALTCHECK_URL, body)
+  .then((response) => {
     if (response.status === 200) {
       console.log("Healthcheck sent successfully");
     } else {
-      console.log(`Failed to send healthcheck. Status code: ${response.status}`);
+      console.log(
+        `Failed to send healthcheck. Status code: ${response.status}`
+      );
     }
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(`An error occurred: ${error.message}`);
   });
