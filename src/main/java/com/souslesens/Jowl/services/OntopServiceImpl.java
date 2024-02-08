c2babfeaecf107fb583403b856f5d6ec69ee95db
hrssilva <haroldo.rssilva@gmail.com>
2024-02-08 07:17:45 -0300

    functionally working, review env variables for jdbc and runtime errors

--- src/main/java/com/souslesens/Jowl/services/OntopServiceImpl.java
+++ src/main/java/com/souslesens/Jowl/services/OntopServiceImpl.java
@@ -1,4 +1,5 @@
 package com.souslesens.Jowl.services;
+
 import com.souslesens.Jowl.model.ontopSparqlToSqlInput;
 import com.souslesens.Jowl.model.ontopRepoDataInput;
 
@@ -19,83 +20,82 @@ import java.util.Base64;
 
 import java.util.HashMap;
 
-
 @Service
 public class OntopServiceImpl implements OntopService {
-    private static final String propertiesFile = "src/main/resources/application.properties";
+  private static final String propertiesFile = "src/main/resources/ontop.properties";
 
-    @Override
-    public String ontopSPARQL2SQL(String reqEncoded64, OntopRepository repo) {
-        String req = OntopService.decodeBase64(reqEncoded64);
+  @Override
+  public String ontopSPARQL2SQL(String reqEncoded64, OntopRepository repo) {
+    String req = OntopService.decodeBase64(reqEncoded64);
 
-        try (OntopRepositoryConnection connection = repo.getConnection()) {
-            connection.begin();
-            try(TupleQueryResult tupleSql = connection.prepareTupleQuery(QueryLanguage.SPARQL, req).evaluate()){
-                while(tupleSql.hasNext()){
-                    BindingSet bindingSet = tupleSql.next();
-                    for(String bindingName : tupleSql.getBindingNames()){
-                        System.out.println(bindingName + " = " + bindingSet.getValue(bindingName));
-                    }
-                }
-            }
-            connection.commit();
-            return "";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    try {
+      OntopRepositoryConnection connection = repo.getConnection();
+      TupleQueryResult tupleData = connection.prepareTupleQuery(QueryLanguage.SPARQL, req).evaluate();
+      // connection.begin();
+      while (tupleData.hasNext()) {
+        BindingSet bindingSet = tupleData.next();
+        for (String bindingName : tupleData.getBindingNames()) {
+          System.out.println(bindingName + " = " + bindingSet.getValue(bindingName));
         }
+      }
+      // connection.commit();
+      return connection.reformulateIntoNativeQuery(req);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  public OntopRepository ontopInitRepo(ontopRepoDataInput input) {
+    String ontologyContent = OntopService.decodeBase64(input.getOntologyContentEncoded64());
+    String mappings = OntopService.decodeBase64(input.getMappingsEncoded64());
+
+    try {
+      OntopSQLOWLAPIConfiguration config = OntopSQLOWLAPIConfiguration.defaultBuilder()
+          .ontologyReader(new StringReader(ontologyContent))
+          .nativeOntopMappingReader(new StringReader(mappings))
+          .propertyFile(propertiesFile)
+          .build();
+
+      OntopRepository ontopRepo = OntopRepository.defaultRepository(config);
+
+      // verify later
+      try (OntopRepositoryConnection connection = ontopRepo.getConnection()) {
+        // connection.begin();
+        // connection.add(new StringReader(ontologyContent), "", RDFFormat.TURTLE);
+        // connection.commit();
+        // connection.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+      }
+
+      return ontopRepo;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  public boolean healthCheck(HashMap<String, OntopRepository> repos) {
+    boolean allHealthy = true;
+
+    for (HashMap.Entry<String, OntopRepository> entry : repos.entrySet()) {
+      String repoName = entry.getKey();
+      OntopRepository repo = entry.getValue();
+
+      try (OntopRepositoryConnection connection = repo.getConnection()) {
+        System.out.println("Repo " + repoName + "' ok ");
+      } catch (Exception e) {
+        allHealthy = false;
+        System.err.println("Error connecting to '" + repoName + "': " + e.getMessage());
+      }
     }
 
-    @Override
-    public OntopRepository ontopInitRepo(ontopRepoDataInput input) {
-        String ontologyContent = OntopService.decodeBase64(input.getOntologyContentEncoded64());
-        String mappings = OntopService.decodeBase64(input.getMappingsEncoded64());
-
-        try {
-            OntopSQLOWLAPIConfiguration config = OntopSQLOWLAPIConfiguration.defaultBuilder()
-                    .ontologyReader(new StringReader(ontologyContent))
-                    .nativeOntopMappingReader(new StringReader(mappings))
-                    .propertyFile(propertiesFile)                    
-                    .build();
-
-            OntopRepository ontopRepo = OntopRepository.defaultRepository(config);
-
-            // verify later
-            try (OntopRepositoryConnection connection = ontopRepo.getConnection()) {
-              //  connection.begin();
-              //  connection.add(new StringReader(ontologyContent), "", RDFFormat.TURTLE);
-              //  connection.commit();
-              //  connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-
-            return ontopRepo;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public boolean healthCheck(HashMap<String, OntopRepository> repos) {
-        boolean allHealthy = true;
-
-        for (HashMap.Entry<String, OntopRepository> entry : repos.entrySet()) {
-            String repoName = entry.getKey();
-            OntopRepository repo = entry.getValue();
-
-            try (OntopRepositoryConnection connection = repo.getConnection()) {
-                System.out.println("Repo " + repoName + "' ok ");
-            } catch (Exception e) {
-                allHealthy = false;
-                System.err.println("Error connecting to '" + repoName + "': " + e.getMessage());
-            }
-        }
-
-        return allHealthy;
-    }
+    return allHealthy;
+  }
 
 }
